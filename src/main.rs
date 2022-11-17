8f4f53661418e327c03a361e7ab6737a1a449e90
143mailliw <wwokwilliam@gmail.com>
2022-11-17 14:43:43 -0700

    feat: test db connection

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
+mod db;
 mod entities;
 
 use actix_web::{guard, web, App, HttpResponse, HttpServer};
-use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Schema};
+use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Schema, Context};
 use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
+use db::set_up_db;
+use dotenv;
+use sea_orm::{ConnectionTrait, DatabaseConnection, DbBackend};
 use std::io::Result;
 
 struct Query;
 
 #[async_graphql::Object]
 impl Query {
-    async fn howdy(&self) -> &'static str {
-        "partner"
+    async fn howdy(&self, ctx: &Context<'_>) -> &str {
+        let db = ctx.data::<DatabaseConnection>().unwrap();
+        match db.get_database_backend() {
+            DbBackend::Postgres => "Correct",
+            _ => "Wrong"
+        }
     }
 }
 
@@ -33,13 +41,20 @@ async fn gql_playgound() -> HttpResponse {
 
 #[actix_web::main]
 async fn main() -> Result<()> {
+    dotenv::dotenv().ok();
+
+    let db = match set_up_db().await {
+        Ok(db) => db,
+        Err(err) => panic!("fatal: {} ", err)
+    };
+
+    let schema = Schema::build(Query, EmptyMutation, EmptySubscription)
+        .data(db)
+        .finish();
+
     HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(Schema::new(
-                Query,
-                EmptyMutation,
-                EmptySubscription,
-            )))
+            .app_data(web::Data::new(schema.clone()))
             .service(web::resource("/").guard(guard::Post()).to(index))
             .service(web::resource("/").guard(guard::Get()).to(gql_playgound))
     })
