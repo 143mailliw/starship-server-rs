e54d44cb18006ef296da47c626c6c2b396b116bb
143mailliw <wwokwilliam@gmail.com>
2023-03-21 00:12:12 +0000

    refactor(guards): use map_err and ok_or

--- src/guards/session.rs
+++ src/guards/session.rs
@@ -23,30 +23,23 @@ impl SessionGuard {
 #[async_trait::async_trait]
 impl Guard for SessionGuard {
     async fn check(&self, ctx: &Context<'_>) -> Result<(), Error> {
-        let session = ctx.data::<Session>();
+        let session = ctx
+            .data::<Session>()
+            .map_err(|_| errors::create_internal_server_error(None, "NO_SESSION_ERROR"))?;
 
-        match session {
-            Ok(session) => match session.user.as_ref() {
-                Some(user) => {
-                    if self.session_type == SessionType::Admin && !user.admin {
-                        Err(errors::create_forbidden_error(None, "NOT_GLOBAL_ADMIN"))
-                    } else if self.session_type == SessionType::NotBanned && user.banned {
-                        Err(errors::create_forbidden_error(None, "USER_BANNED"))
-                    } else if self.session_type != SessionType::Token && !session.verified {
-                        Err(errors::create_forbidden_error(None, "UNVERIFIED_TOKEN"))
-                    } else {
-                        Ok(())
-                    }
-                }
-                None => Err(errors::create_forbidden_error(
-                    Some("Not logged in."),
-                    "NOT_LOGGED_IN",
-                )),
-            },
-            Err(_error) => Err(errors::create_internal_server_error(
-                None,
-                "NO_SESSION_ERROR",
-            )),
+        let user = session.user.as_ref().ok_or(errors::create_forbidden_error(
+            Some("Not logged in."),
+            "NOT_LOGGED_IN",
+        ))?;
+
+        if self.session_type == SessionType::Admin && !user.admin {
+            Err(errors::create_forbidden_error(None, "NOT_GLOBAL_ADMIN"))
+        } else if self.session_type == SessionType::NotBanned && user.banned {
+            Err(errors::create_forbidden_error(None, "USER_BANNED"))
+        } else if self.session_type != SessionType::Token && !session.verified {
+            Err(errors::create_forbidden_error(None, "UNVERIFIED_TOKEN"))
+        } else {
+            Ok(())
         }
     }
 }
