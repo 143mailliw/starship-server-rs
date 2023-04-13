76f6087379e80bbf2ff8cd76c92599d64f59c348
143mailliw <wwokwilliam@gmail.com>
2023-04-13 20:50:01 +0000

    feat(components): finish resolvers for new info

--- src/entities/graphql/planet_component.rs
+++ src/entities/graphql/planet_component.rs
@@ -1,17 +1,17 @@
 use super::super::planet;
-use super::super::planet_component::Model;
+use super::super::planet_component;
 use crate::errors;
 use async_graphql::types::ID;
 use async_graphql::{Context, Error, Object};
 use chrono::NaiveDateTime;
-use sea_orm::{DatabaseConnection, ModelTrait};
+use sea_orm::{DatabaseConnection, EntityTrait, ModelTrait};
 
 #[Object(
     name = "PlanetComponent",
     rename_fields = "camelCase",
     rename_args = "camelCase"
 )]
-impl Model {
+impl planet_component::Model {
     #[graphql(complexity = 0)]
     async fn id(&self) -> ID {
         ID(self.id.clone())
@@ -49,5 +49,24 @@ impl Model {
                 None,
                 "PLANET_MISSING_ERROR",
             ))
+    }
+
+    #[graphql(complexity = 5)]
+    async fn parent(&self, ctx: &Context<'_>) -> Result<Option<planet_component::Model>, Error> {
+        let db = ctx.data::<DatabaseConnection>().unwrap();
+
+        if let Some(parent) = self.parent_id.clone() {
+            planet_component::Entity::find_by_id(parent)
+                .one(db)
+                .await
+                .map_err(|_| errors::create_internal_server_error(None, "FIND_PARENT_ERROR"))
+        } else {
+            Ok(None)
+        }
+    }
+
+    #[graphql(complexity = 0)]
+    async fn position(&self) -> i32 {
+        self.position
     }
 }
