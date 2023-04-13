1e85271640626ea55afb1669361a195201f292c7
143mailliw <wwokwilliam@gmail.com>
2023-04-13 23:39:55 +0000

    feat(components): add deleteComponent

--- src/components/index.rs
+++ src/components/index.rs
@@ -1,12 +1,26 @@
-use crate::errors::create_user_input_error;
+use crate::errors;
 use async_graphql::Error;
 
-pub fn create_component(component: &str, _planet: String, _owner: String) -> Result<String, Error> {
+pub async fn create_component(
+    component: &str,
+    _planet: String,
+    _owner: String,
+) -> Result<String, Error> {
     match component {
         "dummy" => Ok("dummy".to_string()),
-        _ => Err(create_user_input_error(
+        _ => Err(errors::create_user_input_error(
             "That component type isn't valid.",
             "INVALID_TYPE",
+        )),
+    }
+}
+
+pub async fn delete_component(component: &str, _id: String) -> Result<bool, Error> {
+    match component {
+        "dummy" => Ok(true),
+        _ => Err(errors::create_internal_server_error(
+            None,
+            "UNSUPPORTED_COMPONENT",
         )),
     }
 }
--- src/mutations/components.rs
+++ src/mutations/components.rs
@@ -1,4 +1,4 @@
-use crate::components::index::create_component;
+use crate::components::index::{create_component, delete_component};
 use crate::entities::planet_component;
 use crate::errors;
 use crate::permissions::util;
@@ -13,6 +13,7 @@ pub struct ComponentMutation;
 #[Object(rename_fields = "camelCase", rename_args = "camelCase")]
 impl ComponentMutation {
     /// Creates a component.
+    #[graphql(complexity = 50)]
     async fn create_component(
         &self,
         ctx: &Context<'_>,
@@ -30,7 +31,7 @@ impl ComponentMutation {
         util::check_permission("planet.component.create", &planet, member, roles)?;
 
         let component_id =
-            create_component(component.as_str(), planet_id.to_string(), user_id.unwrap())?;
+            create_component(component.as_str(), planet_id.to_string(), user_id.unwrap()).await?;
 
         let position = planet_component::Entity::find()
             .order_by_asc(planet_component::Column::Position)
@@ -61,6 +62,48 @@ impl ComponentMutation {
             .await
             .map_err(|_| errors::create_internal_server_error(None, "RETRIEVAL_ERROR"))?
             .ok_or(errors::create_internal_server_error(None, "MISSING_ERROR"))
+    }
+
+    /// Deletes a component.
+    #[graphql(complexity = 200)]
+    async fn delete_component(
+        &self,
+        ctx: &Context<'_>,
+        id: ID,
+        token: Option<u32>,
+    ) -> Result<bool, Error> {
+        let db = ctx.data::<DatabaseConnection>().unwrap();
+        let session = ctx.data::<Session>().unwrap();
+        let user_id = session.user.as_ref().map(|user| user.id.clone());
+
+        let component = planet_component::Entity::find_by_id(id.to_string())
+            .one(db)
+            .await
+            .map_err(|_| errors::create_internal_server_error(None, "RETRIEVAL_ERROR"))?
+            .ok_or(errors::create_not_found_error())?;
+
+        let planet = util::get_planet(component.clone().planet, db).await?;
+        let member = util::get_planet_member(user_id, component.clone().planet, db).await?;
+        let roles = util::get_member_roles(member.clone(), db).await?;
+        util::check_permission("planet.component.rename", &planet, member, roles)?;
+
+        util::verify_token(db, session.user.as_ref().unwrap(), token).await?;
+        if planet.home == Some(component.id.clone()) {
+            return Err(errors::create_user_input_error(
+                "You can't delete the home component.",
+                "HOME",
+            ));
+        }
+
+        delete_component(&component.r#type, component.id.clone()).await?;
+
+        let active_component: planet_component::ActiveModel = component.into();
+
+        active_component
+            .delete(db)
+            .await
+            .map_err(|_| errors::create_internal_server_error(None, "DELETE_PLANET_ERROR"))
+            .map(|_| true)
     }
 
     /// Renames a component.
