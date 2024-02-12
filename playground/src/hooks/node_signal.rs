a82b3b1d24e5b1d2a567b35b19121049d6bab149
143mailliw <wwokwilliam@gmail.com>
2024-02-12 14:16:47 -0700

    refactor: rename toolbox-types

---
+++ playground/src/hooks/node_signal.rs
@@ -0,0 +1,5 @@
+use leptos::{create_signal, ReadSignal};
+
+pub fn create_node_signal() -> ReadSignal<String> {
+    panic!("a");
+}
---
+++ playground/src/main.rs
@@ -0,0 +1,23 @@
+pub mod hooks;
+
+use leptos::*;
+
+fn main() {
+    mount_to_body(|| view! { <App/> })
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button
+            on:click=move |_| {
+                set_count(3);
+            }
+        >
+            "Click me: "
+            {move || count.get()}
+        </button>
+    }
+}
